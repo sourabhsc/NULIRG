262ff69e961416e08d307d5105b8d80d2f923b9f
sourabh <sourabh@astro.umn.edu>
2018-09-21 20:09:26 -0500

    set up files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from io import open
+from setuptools import setup, find_packages
+from os import path
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='NULIRG',
+    version='0.1dev',
+    description='NULIRG documentation',
+    author='Sourabh Chauhan',
+    packages=['NULIRG'],
+    include_package_data=True,
+
+    package_data={'NULIRG': ['data/*', 'data_dark/*']},
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.rst').read(),
+)
