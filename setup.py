dac43548e990f44622255b32ff07de71e04ab2a4
sourabh <sourabh@astro.umn.edu>
2018-09-30 02:38:45 -0500

    versioneer added to setup

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from io import open
 from setuptools import setup, find_packages
 from os import path
 here = path.abspath(path.dirname(__file__))
-
+import NULIRG
+import versioneer
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='NULIRG',
-    version='0.1dev',
+    version=versioneer.get_version(),
+    # version=NULIRG.__version__,
+    cmdclass=versioneer.get_cmdclass(),
     description='NULIRG documentation',
     author='Sourabh Chauhan',
     packages=['NULIRG'],
@@ -18,5 +21,5 @@ setup(
     author_email=['sourabh@astro.umn.edu'],
     package_data={'NULIRG': ['data/*', 'data_dark/*']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.rst').read(),
+    long_description=open('README.md').read(),
 )
